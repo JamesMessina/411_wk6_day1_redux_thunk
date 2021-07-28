fdf11c83fe44a922ab1b1d860d1abe97d61d2833
James Messina <jmessina17@gmail.com>
2021-07-28 15:56:15 -0500

    still working

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container } from '@material-ui/core'
+
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <Container>
+            <p>new component!</p>
+        </Container>
     )
 }
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -13,3 +13,17 @@ export const removeCar = (index) => {
         value: index
     }
 }
+
+export const fetchMakes = () =>{
+    return(dispatch) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(response => {
+                const makesAction = {
+                    type: 'FETCH_MAKES',
+                    value: response.Results
+                }
+                dispatch(makesAction)
+            })
+    }
+}
