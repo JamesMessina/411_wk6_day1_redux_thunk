becea293247815625a24bdce0283bbd945aca2eb
James Messina <jmessina17@gmail.com>
2021-07-31 02:09:25 -0500

    mapped data to import component

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        marginTop: '20px'
+      },
+    },
+}));
+
 
 
 const Import = (props) => {
-    // fill out this component
+    const classes = useStyles();
 
     return (
-        <Container>
-            <p>new component!</p>
+        <Container className={classes.root}>
+            <Button onClick={props.fetchMakes} variant="contained" color="primary">
+                Import
+            </Button>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Make</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.makes.map((make, idx) => (
+                        <TableRow key={make.MakeId}>
+                            <TableCell>{make.MakeId}</TableCell>
+                            <TableCell>{make.MakeName}</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                ))}
+                </TableBody>
+            </Table>
         </Container>
     )
 }
