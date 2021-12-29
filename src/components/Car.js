a5e782570247d43fc066a32eddd976c1c9a41291
James Messina <jmessina17@gmail.com>
2021-12-29 11:59:15 -0600

    made back button functional for cars

--- src/components/Car.js
+++ src/components/Car.js
@@ -2,27 +2,26 @@ import React from 'react'
 import { Container, Paper, Chip, Typography  } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import IconButton from '@material-ui/core/IconButton';
-//import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 
 
 const Car = (props) => {
     const id = props.match.params.id
-    {/* Change cars to props.cars and remove the cars.json import above */}
     const car = props.cars.find(c => c.id == id);
 
-    //const history = useHistory(); 
+    const history = useHistory(); 
 
     const handleClick = () => {
         console.log("back button clicked. useHistory hook not working. cant exoprt from react router dom.");
-        // history.push("/")
+        history.push("/")
     }
 
     return (
         <Container maxWidth="sm" className="car-container">
             <Paper className="car-paper">
                 <div style={{display: "flex", alignItems: "center"}}>
-                    <IconButton onClick={handleClick}>
+                    <IconButton onClick={() => handleClick()}>
                         <ArrowBackIcon style={{marginRight: "5px"}} color="primary" />
                     </IconButton>
                     <Typography>Back</Typography>
