daf83a2d20c6d1d006c981b8094bb8ea5aafd8aa
James Messina <jmessina17@gmail.com>
2024-09-28 17:18:40 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/411_wk6_day1_redux_thunk/dashboard/precache-manifest.bb3178e41c72553c684ba2110d54c851.js"
+  "/411_wk6_day1_redux_thunk/precache-manifest.0735ebc026cb0a0ce074e6899e7c95a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/411_wk6_day1_redux_thunk/dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/411_wk6_day1_redux_thunk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
